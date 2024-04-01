b160a8b0f81420d508136a7c207f5fa5f07fd199
youssefsenkaoui <youssef.senkaoui29@gmail.com>
2024-04-01 11:14:38 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
